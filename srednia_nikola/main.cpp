796d3cbd04d0d6dd7e759aad98fde346e6bf7fab
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-01-17 19:04:44 +0100

    dodaje petle dla nikoli

---
+++ srednia_nikola/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int i,x;
+double suma;
+
+int main(){
+
+for(i=1;i<14;i++){
+cout<<"wprowadz liczbe: ";
+cin >>x;
+suma=suma+x;
+
+}
+cout <<"srednia arytmetyczna podanych liczb to: "<<(suma/13)<<endl;
+
+    return 0;
+}

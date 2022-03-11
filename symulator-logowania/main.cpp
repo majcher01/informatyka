093b17140979c5659fba1ec6927a363874c5c2df
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-03-11 13:27:32 +0100

    zaczety symuator logowania

---
+++ symulator-logowania/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+string login="andrzej", haslo="hasloandrzeja";;
+string ulogin="", uhaslo="";
+int x;
+using namespace std;
+
+int main(){
+
+
+    do{
+        cout<<"podaj login: ";
+        cin>>ulogin;
+        cout<<endl<<"podaj haslo: ";
+        cin>>uhaslo;
+
+        if(ulogin==login){
+            x=1;
+        }
+        if
+    }while();
+
+
+    return 0;
+}

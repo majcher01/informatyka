a48961d73f38f7052e4635aaf4b90370a2b32705
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-03-11 13:16:34 +0100

    zrobione zadanie

--- while-1103/main.cpp
+++ while-1103/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+int a,b;
 using namespace std;
 
 int main(){
+
+    do{
+        cout<<"podaj a: ";
+        cin>>a;
+        cout<<endl<<"podaj b: ";
+        cin>>b;
+
+        cout<<endl<<a+b<<endl;
+    }while((a+b)>0);
+
+    cout<<"suma nie jest wieksza od zera, koniec"<<endl;
 
 
     return 0;

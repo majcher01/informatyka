86335acc6890a7b5bcb508736435b9127d7bb210
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-03-18 13:09:28 +0100

    zadanie-rysowanie

---
+++ rysowanie/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout <<"kwadrat"<<endl<<endl;
+
+    for(int i=0;i<5;i++){
+
+        for(int j=0;j<5;j++){
+            cout<<"# ";
+        }
+
+        cout<<endl;
+    }
+
+    cout<<endl<<endl<<"prostokat 1"<<endl<<endl;
+
+    for(int i=0;i<5;i++){
+
+        for(int j=0;j<8;j++){
+            cout<<"# ";
+        }
+
+        cout<<endl;
+    }
+
+    cout<<endl<<endl<<"prostokat 2"<<endl<<endl;
+
+    for(int i=0;i<8;i++){
+
+        for(int j=0;j<5;j++){
+            cout<<"# ";
+        }
+
+        cout<<endl;
+    }
+
+
+
+
+    return 0;
+}

cabde21c47a266390bb9057f10cee75db0a9f0c5
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-04-22 13:32:29 +0200

    prawie gotowe

--- tablice-2204/main.cpp
+++ tablice-2204/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int tablica[]={49,13,1,52,-2,-22,0,23,4,-6,23,111,-52};
-
+int y;
 int main(){
 
 cout<<"Zawartosc tablicy:"<<endl;
@@ -13,7 +13,50 @@ for(int x : tablica){
 
 tablica[3]=100, tablica[5]=-33;
 
-cout<<"Elementy 3 i 5 po zmianie:"<<endl<<tablica[3]<<" "<<tablica[5]<<endl;
+cout<<endl<<"Tablica po zmianie:"<<endl;
+for(int x : tablica){
+    cout<<x<<", ";
+}
+
+cout<<endl<<"Dodatnie elementy:"<<endl;
+
+for(int x : tablica){
+    if(x>0){
+        cout<<x<<", ";
+    }
+}
+
+cout<<endl<<"Ilosc ujemnych elementów:"<<endl;
+
+for(int x : tablica){
+    if(x<0){
+        y=y+1;
+    }
+}
+cout<<y;
+
+y=0;
+
+cout<<endl<<"Elementy mniejsze od 10:"<<endl;
+
+for(int x : tablica){
+    if(x<10){
+        cout<<x<<", ";
+    }
+}
+cout<<y;
+
+y=0;
+
+cout<<endl<<"Suma wszystkich elementow:"<<endl;
+
+for(int x : tablica){
+    y=y+x;
+}
+cout<<y;
+
+cout<<endl<<"Srednia:"<<endl<<(y/13)<<endl;
+
 
 
     return 0;

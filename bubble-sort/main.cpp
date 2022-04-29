3f09c22e51470e68c64741d35de0e9dfd10cea68
majcher01 <majcherski.jakub@gmail.com>
2022-04-29 22:30:38 +0200

    koniec

--- bubble-sort/main.cpp
+++ bubble-sort/main.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 void bubblesort(int tab[],int n)
 {
-	for(int i=0;i<n;i++)
-		for(int j=1;j<n-i;j++) 
+	for(int i=0;i<n;i++){
+		for(int j=1;j<n-i;j++) {
 		if(tab[j-1]>tab[j])
 		swap(tab[j-1], tab[j]);
+        }
+    }
+
 }
 
 int main()
 {
 		
-	int tab[] = {5,1,7,24,7,1,2,4}; //tablica do posortowania
+	int tab[] = {5,1,7,24,7,1,2,4,33,8}; //tablica do posortowania
  
-    int n =8;   //dlugosc tablicy
+    int n = 10;   //dlugosc tablicy
     
 	
 	bubblesort(tab,n);

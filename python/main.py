9763eb7d3d3d7270e47669daa0c5417d1bd42429
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-10-11 14:14:24 +0200

    pytong

--- python/main.py
+++ python/main.py
@@ -1,1 +1,10 @@
-print('dupa')
+a = int(input("podaj dlugosc boku a"))
+b = int(input("podaj dlugosc boku b"))
+c = int(input("podaj kat miedzy bokami"))
+
+if(a!=0 and b!=0 and c==45):
+    if(a==b):
+        print('mozesz stworzyc romb')
+else:
+        print('nie mozesz stworzyc rombu')
+
